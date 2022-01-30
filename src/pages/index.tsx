8e3ac060e093e9710ec9a31f05c0ddf1ab516148
EduardoReisUX <ducabruno09@hotmail.com>
2022-01-30 15:18:38 -0400

    Home page get data from FaunaDB, format and displays it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,8 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
-function images() {
-  const fetchImages = ({ pageParam = 0 }) =>
-    api.get(`/images?after=${pageParam}`);
-}
+const fetchImages = ({ pageParam = 1 }) =>
+  api.get(`/images?after=${pageParam}`);
 
 export default function Home(): JSX.Element {
   const {
@@ -24,26 +22,52 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
-    images
+    fetchImages,
     // TODO GET AND RETURN NEXT PAGE PARAM
+    {
+      staleTime: 1000,
+    }
   );
+
+  console.log(data);
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    if (data === undefined) return;
+
+    const newData = data.pages[0].data.data.map(imageFromFauna => ({
+      title: imageFromFauna.title,
+      description: imageFromFauna.description,
+      url: imageFromFauna.url,
+      ts: imageFromFauna.ts,
+      id: imageFromFauna.id,
+    }));
+
+    return newData;
   }, [data]);
-
-  // TODO RENDER LOADING SCREEN
-
-  // TODO RENDER ERROR SCREEN
 
   return (
     <>
       <Header />
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        {isLoading ? <Loading /> : isError ? <Error /> : 'Carregou sem erro'}
-        <CardList /* cards={formattedData} */ />
+        {
+          // TODO RENDER LOADING SCREEN
+          // TODO RENDER ERROR SCREEN
+          isLoading ? (
+            <Loading />
+          ) : isError ? (
+            <Error />
+          ) : (
+            <CardList cards={formattedData} />
+          )
+        }
+
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
-        {hasNextPage && <Button>Carregar mais</Button>}
+        {hasNextPage && (
+          <Button disabled={isFetchingNextPage} onClick={() => fetchNextPage()}>
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
