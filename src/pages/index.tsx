a30749bc4c709c106f04cda70786fe1c9c854f7e
EduardoReisUX <ducabruno09@hotmail.com>
2022-02-01 16:19:41 -0400

    fix axios get

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,27 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
-const fetchImages = ({ pageParam = null }) =>
-  !!pageParam ? api.get(`/images?after=${pageParam}`) : api.get('/images');
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface GetImagesResponse {
+  after: string;
+  data: Image[];
+}
+
+const fetchImages = ({ pageParam = null }): Promise<GetImagesResponse> =>
+  api
+    .get('api/images', {
+      params: {
+        after: pageParam,
+      },
+    })
+    .then(response => response.data);
 
 export default function Home(): JSX.Element {
   const {
@@ -19,58 +38,33 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    fetchImages,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-    {
-      getNextPageParam: lastPage => lastPage.data?.after,
-    }
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage?.after,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
-    if (!data) return;
+    const formatted = data?.pages.flatMap(imageData => imageData.data.flat());
 
-    const dataFromPages = data.pages.map(page => {
-      return page.data.data.map(image => ({
-        title: image.title,
-        description: image.description,
-        url: image.url,
-        ts: image.ts,
-        id: image.id,
-      }));
-    });
-
-    const dataFromAllPages = dataFromPages.reduce((prevPage, currentPage) => [
-      ...prevPage,
-      ...currentPage,
-    ]);
-
-    const index: number = data.pages.length - 1;
-    const after: number = data.pages[index]?.data.after || null;
-
-    return {
-      after,
-      dataFromAllPages,
-    };
+    return formatted;
   }, [data]);
 
-  return isLoading ? (
-    <Loading />
-  ) : isError ? (
-    <Error />
-  ) : (
+  if (isLoading && !isError) {
+    return <Loading />;
+  }
+
+  if (!isLoading && isError) {
+    return <Error />;
+  }
+
+  return (
     <>
       <Header />
-      <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData.dataFromAllPages} />
 
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+      <Box maxW={1120} px={20} mx="auto" my={20}>
+        <CardList cards={formattedData} />
         <Button
           disabled={!hasNextPage || isFetchingNextPage}
-          onClick={() => fetchNextPage({ pageParam: formattedData.after })}
+          onClick={() => fetchNextPage()}
         >
           {isFetchingNextPage
             ? 'Carregando...'
